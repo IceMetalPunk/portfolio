9fa03f3721d43a5248daf1804bd00ad093861b15
IceMetalPunk <IceMetalPunk@gmail.com>
2025-07-17 06:23:58 -0400

    Complete basic sidebar functionality

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -1,15 +1,53 @@
-import { useState } from 'react';
+import classNames from 'classnames';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const Sidebar = () => {
-  const [sidebarWidth, setSidebarWidth] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpenerVisible, setIsOpenerVisible] = useState<boolean>(true);
+  const sidebarRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (sidebarRef.current) {
+      const openerShower = () => setIsOpenerVisible(!isOpen);
+      sidebarRef.current.addEventListener('transitionend', openerShower);
+      return () =>
+        sidebarRef.current?.removeEventListener('transitionend', openerShower);
+    }
+  }, [sidebarRef.current, isOpen, setIsOpenerVisible]);
+
+  const setOpen = useCallback(
+    (opened: boolean) => {
+      setIsOpen(opened);
+      if (opened) {
+        setIsOpenerVisible(false);
+      }
+    },
+    [setIsOpen, setIsOpenerVisible]
+  );
+
   return (
-    <section
-      id='sidebar'
-      style={{ width: `${sidebarWidth}px` }}
-      onMouseOver={() => setSidebarWidth(100)}
-      onMouseOut={() => setSidebarWidth(0)}
-    >
-      Sidebar Here
+    <section id='sidebar-container'>
+      {isOpenerVisible && (
+        <a onClick={() => setOpen(true)} className='opener link'>
+          ☰
+        </a>
+      )}
+      <section
+        ref={sidebarRef}
+        id='sidebar'
+        className={classNames({
+          open: isOpen,
+        })}
+      >
+        <a onClick={() => setOpen(false)} className='link'>
+          ☰
+        </a>
+        <ul>
+          <li>Synthia Nova</li>
+          <li>Compliance</li>
+          <li>Godform</li>
+        </ul>
+      </section>
     </section>
   );
 };
